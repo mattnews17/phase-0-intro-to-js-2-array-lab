40ae826f6c2cd643a6d1c726496dfd5cbb2255b2
Matthew Newsome <matthew90020@gmail.com>
2022-10-26 19:32:05 -0400

    commit tree 400a54752d7464e1557f20631edff73be55c2ba7

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+var cats =['Milo', 'Otis', 'Garfield']
+const destructivelyAppendCat= (name)=>
+{
+    cats.push(name);
+    return cats;
+}
+const destructivelyPrependCat =(name)=>{
+    cats.unshift(name);
+    return cats;
+}
+const destructivelyRemoveLastCat = ()=>
+{
+    cats.pop();
+    return cats;
+}
+
+ const destructivelyRemoveFirstCat=()=>{
+     cats.shift();
+     return cats;
+ }
+ const appendCat=(name)=>{
+     var new_array = cats.slice();
+     new_array.push(name);
+     return new_array;
+ }
+ const prependCat=(name)=>{
+     var new_array = cats.slice();
+     new_array.unshift(name);
+     return new_array;
+ }
+const removeLastCat=()=>{
+   var new_array =cats.slice();
+   new_array.pop();
+   return new_array; 
+}
+const removeFirstCat =()=>{
+    var new_array = cats.slice(1);
+    return new_array;
+}
